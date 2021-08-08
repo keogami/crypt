6444959a16f6c04517c81019327058c5bdeb35ff
keogami <41939011+keogami@users.noreply.github.com>
2021-08-08 20:22:20 +0530

    passing empty string to -p initiates terminal input

--- app.go
+++ app.go
@@ -1,17 +1,37 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	cli "github.com/urfave/cli/v2"
+	"golang.org/x/term"
 )
+
+func inputPassphrase(ctx *cli.Context) error {
+	if ctx.String(passphraseOption) != "" {
+		return nil
+	}
+
+	fmt.Print("Input Passphrase: ")
+
+	p, err := term.ReadPassword(int(os.Stdin.Fd()))
+	if err != nil {
+		return nil
+	}
+
+	return ctx.Set("passphrase", string(p))
+}
 
 func makeApp() *cli.App {
 	return &cli.App{
-		Name:  "crypt",
-		Usage: "encrypt and decrypt files with AES-256",
+		Name:   "crypt",
+		Usage:  "encrypt and decrypt files with AES-256",
+		Before: inputPassphrase,
 		Flags: []cli.Flag{
 			&cli.StringFlag{
 				Name:     "passphrase",
-				Usage:    "passphrase to be used for operations",
+				Usage:    "passphrase to be used for operations; if empty, you will be prompted to type it in",
 				Aliases:  []string{"pass", "p"},
 				Required: true,
 			},
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.15
 require (
 	github.com/urfave/cli/v2 v2.3.0
 	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b
+	golang.org/x/term v0.0.0-20201126162022-7de9c90e9dd1
 )
