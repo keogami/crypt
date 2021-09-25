68ba7805b8b3165e7bda28e353d09f0876b001cf
keogami <41939011+keogami@users.noreply.github.com>
2021-09-25 15:27:19 +0530

    created a ReadFile() func to map os errors to user-friendly errors

--- commons.go
+++ commons.go
@@ -3,6 +3,10 @@ package main
 import (
 	"crypto/aes"
 	"crypto/cipher"
+	"errors"
+	"os"
+
+	cli "github.com/urfave/cli/v2"
 )
 
 // SaltGen is a func that generates salt on demand, only exist to make the signatures less verbose
@@ -32,4 +36,18 @@ func NewAESGCM(pass string, sg SaltGen) (cipher.AEAD, Salt, error) {
 	}
 
 	return gcm, salt, nil
+}
+
+func ReadFile(filename string, errcode int) ([]byte, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		errstr := filename + ": "
+		if errors.Is(err, os.ErrNotExist) {
+			errstr += "file doesn't exist"
+		} else if errors.Is(err, os.ErrPermission) {
+			errstr += "permission denied"
+		}
+		return nil, cli.Exit(errstr, errcode)
+	}
+	return data, nil
 }
