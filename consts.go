317214e4fa5c4e41d006f949fc374e9bc02747bf
keogami <41939011+keogami@users.noreply.github.com>
2021-09-25 14:41:58 +0530

    added more error constants to improve UX

--- consts.go
+++ consts.go
@@ -17,12 +17,22 @@ const (
 	ExitNoArgs
 	ExitNoOutputPath
 	ExitAuthFailed
+	ExitKeyFailed
+	ExitCipherFailed
+	ExitGCMFailed
+	ExitSaltGenFailed
+	ExitSaltLoadFailed
 )
 
 var (
-	ErrPassEmpty    = cli.Exit("password is empty", ExitPassEmpty)
-	ErrSaltEmpty    = cli.Exit("salt file is not provided", ExitSaltEmpty)
-	ErrNoArgs       = cli.Exit("not enough argument provided", ExitNoArgs)
-	ErrNoOutputPath = cli.Exit("no output path provided with empty passphrase", ExitNoOutputPath)
-	ErrAuthFailed   = cli.Exit("authentication failed", ExitAuthFailed)
+	ErrPassEmpty      = cli.Exit("password is empty", ExitPassEmpty)
+	ErrSaltEmpty      = cli.Exit("salt file is not provided", ExitSaltEmpty)
+	ErrNoArgs         = cli.Exit("not enough argument provided", ExitNoArgs)
+	ErrNoOutputPath   = cli.Exit("no output path provided with empty passphrase", ExitNoOutputPath)
+	ErrAuthFailed     = cli.Exit("authentication failed", ExitAuthFailed)
+	ErrKeyFailed      = cli.Exit("key derivation failed", ExitKeyFailed)
+	ErrCipherFailed   = cli.Exit("aes cipher creation failed", ExitCipherFailed)
+	ErrGCMFailed      = cli.Exit("GCM creation failed", ExitGCMFailed)
+	ErrSaltGenFailed  = cli.Exit("salt generation failed", ExitSaltGenFailed)
+	ErrSaltLoadFailed = cli.Exit("salt loading failed", ExitSaltLoadFailed)
 )
